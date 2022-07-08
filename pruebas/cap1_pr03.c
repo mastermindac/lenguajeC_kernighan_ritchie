dfa090d6ad30e4b8062117799a0f2956cbb20d61
pacomastermind <paco@mastermind.ac>
2022-07-08 14:58:11 +0200

    Prueba cap1_pr03

--- pruebas/cap1_pr03.c
+++ pruebas/cap1_pr03.c
@@ -7,12 +7,12 @@
 */
 
 #include <stdio.h>
-// imprime la tabla de F-C para fahr=0,20, ...,300
+// imprime la tabla de F-C para fahr=0,20, ...,300 en sentido inverso
 
 main(){
     int fahr;
 
-    for (fahr=0;fahr<=300; fahr=fahr+20)
+    for (fahr=300;fahr>=0; fahr=fahr-20)
     {
         printf("%3d %6.1f\n",fahr,(5.0/9.0)*(fahr-32));
     }
